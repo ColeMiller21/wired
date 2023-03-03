fa4791e6d0d4433694f052cf1f239ed60204a79a
Cole Miller <cole.t.miller@gmail.com>
2023-03-03 16:56:53 -0600

    updated

--- data/teamInfo.js
+++ data/teamInfo.js
@@ -1,14 +1,14 @@
 export const teamMembers = [
   {
     name: "Overl0rd",
-    image: "/portait-2.png",
+    image: "/p-2.png",
     alt: "Overl0rd",
     title: "Marketer",
     bio: "Introducing 0verl0rd, a seasoned project lead and marketer with a proven track record of success in the blockchain and NFT space. With over 5 years of experience, 0verl0rd has led and advised numerous successful projects, helping them achieve exponential growth and reach new heights in the industry. They are well-versed in community building, influencer outreach, and strategic partnerships, and have a deep understanding of what it takes to create a successful NFT project from inception to launch. 0verl0rd's expertise and dedication to their craft make them an invaluable asset to any project looking to make their mark in the NFT world.",
   },
   {
     name: "Bart",
-    image: "/portait-3.png",
+    image: "/p-3.png",
     alt: "Bart",
     title: "Artist",
     bio: "Bart is an incredibly talented artist with a passion for creating imaginative and visually stunning artwork. As a student in art and design, Bart has been honing their skills for years, perfecting their craft in digital art and character design. They have an eye for detail and specialize in bringing characters and environments to life through vivid colors and imaginative designs. Bart has a strong grasp of the software tools used to create digital art and can produce stunning visuals for a variety of mediums. With their talent and passion, Bart is poised for a successful career in the art world, creating captivating and original works that inspire and delight.",
