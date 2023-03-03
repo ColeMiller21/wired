49380bef951d4e30200682cb5fa40aafd847674a
Cole Miller <cole.t.miller@gmail.com>
2023-03-03 16:46:33 -0600

    updated wl checker

--- components/sections/WLChecker.js
+++ components/sections/WLChecker.js
@@ -32,7 +32,7 @@ const WLChecker = () => {
       console.log("Is whitelisted");
     } catch (err) {
       console.log(err);
-      setIsChecking(false);
+      setIsChecking;
     }
   };
 
@@ -43,28 +43,32 @@ const WLChecker = () => {
   return (
     <div className="w-full min-h-screen flex flex-col items-center sub-page-bg bg-cover bg-no-repeat gap-[2.5rem] relative">
       <MainHeader>WL CHECKER</MainHeader>
-      <div className="relative w-[90%] lg:w-[50%] z-1">
-        <div className="flex flex-col items-center justify-center px-[2rem] py-[3rem] gap-[1.5rem] backdrop-blur-md bg-black bg-opacity-40">
-          <h3 className="text-gray-300 text-center">
-            Enter Address below to check WL status!
-          </h3>
-          <input
-            type="text"
-            placeholder="Enter Address"
-            className="h-[45px] w-[250px] md:w-[400px] border-2 border-red-800 rounded pl-2 text-[#FAFAFA] bg-[#141414] overflow-hidden"
-            onChange={(e) => setAddress(e.target.value)}
-            value={address}
-          />
-          {message !== "" && <p className="text-gray-300">{message}</p>}
-          {isError && <p>{errorMessage}</p>}
-          <button
-            onClick={checkForUser}
-            className="bg-black px-[24px] py-[12px] text-gray-300"
-          >
-            {isChecking ? <ClipLoader color="#FaFafa" size={20} /> : "Check"}
-          </button>
-        </div>
+
+      <div className="flex flex-col items-center justify-center px-[2rem] py-[3rem] gap-[1.5rem] backdrop-blur-md bg-black bg-opacity-40 z-10">
+        <h3 className="text-gray-300 text-center">
+          Enter Address below to check WL status!
+        </h3>
+        <input
+          type="text"
+          placeholder="Enter Address"
+          className="h-[45px] w-[250px] md:w-[400px] border-2 border-red-800 rounded pl-2 text-[#FAFAFA] bg-[#141414] overflow-hidden"
+          onChange={(e) => setAddress(e.target.value)}
+          value={address}
+        />
+        {message !== "" && <p className="text-gray-300">{message}</p>}
+        {isError && <p>{errorMessage}</p>}
+        <button
+          onClick={checkForUser}
+          className="bg-black px-[24px] py-[12px] text-gray-300"
+        >
+          {isChecking ? <ClipLoader color="#FaFafa" size={20} /> : "Check"}
+        </button>
       </div>
+      <img
+        src="/Victim.png"
+        alt="Victim"
+        className="min-w-[250px] md:w-[400px] absolute bottom-0 right-[-5%] z-1"
+      />
     </div>
   );
 };
